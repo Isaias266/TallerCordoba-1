c5944a9129b69a83ae3cdbae53c8c810e2137245
Isaias Avalle <isaiasavalle9@gmail.com>
2019-09-04 09:56:11 -0300

    Se agrega clase Estado

---
+++ TallerAutos/TallerAutos/Clases/Estado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+namespace TallerAutos.Clases
+{
+    class Estados
+    {
+
+        private int codEstado;
+        private string nombre;
+        private string descripcion;
+
+        BaseDeDatos sDB = new BaseDeDatos();
+
+        public DataTable recuperarEstados()
+        {
+            DataTable tabla = new DataTable();
+            tabla = sDB.consultarTabla("Estados");
+            return tabla;
+
+        }
+
+    }
+}
