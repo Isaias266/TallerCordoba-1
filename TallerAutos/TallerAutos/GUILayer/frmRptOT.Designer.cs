fae97f6d91bc871f4d4321b71d04d946a2d1dca7
Isaias Avalle <isaiasavalle9@gmail.com>
2019-10-21 19:36:06 -0300

    Se agrega reporte mensual de OTs

---
+++ TallerAutos/TallerAutos/GUILayer/frmRptOT.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace TallerAutos.GUILayer
+{
+    partial class frmRptOT
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.dataTableOTsBindingSource2 = new System.Windows.Forms.BindingSource(this.components);
+            this.dsRptOT = new TallerAutos.dsRptOT();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.cboMes = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.dataTableOTsBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataTableOTsBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.dataTableOTsTableAdapter = new TallerAutos.dsRptOTTableAdapters.dataTableOTsTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTableOTsBindingSource2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsRptOT)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTableOTsBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTableOTsBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataTableOTsBindingSource2
+            // 
+            this.dataTableOTsBindingSource2.DataMember = "dataTableOTs";
+            this.dataTableOTsBindingSource2.DataSource = this.dsRptOT;
+            // 
+            // dsRptOT
+            // 
+            this.dsRptOT.DataSetName = "dsRptOT";
+            this.dsRptOT.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(467, 44);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerar.TabIndex = 0;
+            this.btnGenerar.Text = "Generar";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click_1);
+            // 
+            // cboMes
+            // 
+            this.cboMes.FormattingEnabled = true;
+            this.cboMes.Location = new System.Drawing.Point(299, 44);
+            this.cboMes.Name = "cboMes";
+            this.cboMes.Size = new System.Drawing.Size(121, 21);
+            this.cboMes.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(172, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(124, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Seleccionar mes reporte:";
+            // 
+            // reportViewer1
+            // 
+            reportDataSource1.Name = "datasetRptOT";
+            reportDataSource1.Value = this.dataTableOTsBindingSource2;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "TallerAutos.Reportes.rptOT2.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(12, 90);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(765, 496);
+            this.reportViewer1.TabIndex = 3;
+            this.reportViewer1.Load += new System.EventHandler(this.reportViewer1_Load);
+            // 
+            // dataTableOTsBindingSource1
+            // 
+            this.dataTableOTsBindingSource1.DataMember = "dataTableOTs";
+            this.dataTableOTsBindingSource1.DataSource = this.dsRptOT;
+            // 
+            // dataTableOTsTableAdapter
+            // 
+            this.dataTableOTsTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmRptOT
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(789, 598);
+            this.Controls.Add(this.reportViewer1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cboMes);
+            this.Controls.Add(this.btnGenerar);
+            this.Name = "frmRptOT";
+            this.Text = "frmRptOT";
+            this.Load += new System.EventHandler(this.frmRptOT_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataTableOTsBindingSource2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsRptOT)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTableOTsBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataTableOTsBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.ComboBox cboMes;
+        private System.Windows.Forms.Label label1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource dataTableOTsBindingSource;
+        private dsRptOTTableAdapters.dataTableOTsTableAdapter dataTableOTsTableAdapter;
+        private System.Windows.Forms.BindingSource dataTableOTsBindingSource1;
+        private dsRptOT dsRptOT;
+        private System.Windows.Forms.BindingSource dataTableOTsBindingSource2;
+    }
+}
---
+++ TallerAutos/TallerAutos/GUILayer/frmRptOT.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TallerAutos.GUILayer
+{
+    public partial class frmRptOT : Form
+    {
+        List<String> meses = new List<String>();
+        private void cargarMeses()
+        {
+            meses.Add("Enero");
+            meses.Add("Febrero");
+            meses.Add("Marzo");
+            meses.Add("Abril");
+            meses.Add("Mayo");
+            meses.Add("Junio");
+            meses.Add("Julio");
+            meses.Add("Agosto");
+            meses.Add("Septiembre");
+            meses.Add("Octubre");
+            meses.Add("Noviembre");
+            meses.Add("Diciembre");
+        }
+    
+    public frmRptOT()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRptOT_Load(object sender, EventArgs e)
+        {
+            this.cargarMeses();
+            this.cboMes.DataSource = this.meses;
+            this.cboMes.SelectedIndex = -1;
+        }
+
+        private void btnGenerar_Click_1(object sender, EventArgs e)
+        {
+            if (this.cboMes.SelectedIndex != -1)
+              {
+                this.dataTableOTsTableAdapter.Fill(dsRptOT.dataTableOTs, this.cboMes.SelectedIndex + 1);
+              }
+              this.reportViewer1.RefreshReport();
+        }
+
+        private void reportViewer1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
